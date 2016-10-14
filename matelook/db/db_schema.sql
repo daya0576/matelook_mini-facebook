c620ce9d6aa3dbfe49408fffb7df2d04100a01c8
daya0576 <daya0576@gmail.com>
2016-10-15 00:50:21 +1100

    profile info done

--- matelook/db/db_schema.sql
+++ matelook/db/db_schema.sql
@@ -1,29 +1,38 @@
-drop table if exists users;
-create table users (
-    'zid' text primary key not null,
-    'email' text not null,
-    'password' text not null
-);
-
-
+-- zid, email, password
 -- 'full_name', 'mates', 'birthday',  'profile_img', 
 -- 'program', 'courses', 
 -- 'home_suburb', 'home_longitude', 'home_latitude'
-drop table if exists user_profile;
-create table user_profile (
+drop table if exists USER;
+create table USER (
     'zid' text primary key not null,
+    'email' text,
+    'password' text not null,
 
     'full_name' text, 
-    'mates' text, 
+    -- 'mates' text, 
     'birthday' text, 
     'profile_img' text, 
 
     'program' text, 
-    'courses' text, 
+    -- 'courses' text, 
 
     'home_suburb' text, 
     'home_longitude' text, 
     'home_latitude' text
 );
 
+drop table if exists MATES;
+CREATE TABLE MATES (
+    id INTEGER PRIMARY KEY   AUTOINCREMENT,
+    zid TEXT REFERENCES USER(zid),
+    mate TEXT
+);
+
+drop table if exists ENROLLMENT;
+CREATE TABLE ENROLLMENT
+(
+    id INTEGER PRIMARY KEY   AUTOINCREMENT,
+    zid TEXT  REFERENCES USER(zid),
+    course TEXT
+);
 
--- matelook/matelook.py
+++ matelook/matelook.py
@@ -1,11 +1,69 @@
-from flask import Flask
+# all the imports
+import os
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
 
+# create our little application :)
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+# Load default config and override config from an environment variable
+app.config.update(dict(
+    PROFILE_IMG_DIR="profile_img",
+    DATABASE=os.path.join(app.root_path, 'db/small_sqlite.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default',
+    TEMPLATES_AUTO_RELOAD=True,
+    DEBUG=True,
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+
+def connect_db():
+    """Connects to the specific database."""
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+def query_db(query, args=(), one=False):
+    """Queries the database and returns a list of dictionaries."""
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    return (rv[0] if rv else None) if one else rv
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/user/<user_zid>')
+def user_profile(user_zid):
+    user_entity = query_db('SELECT * FROM USER WHERE zid = ?',
+                           [user_zid], one=True)
+
+    return render_template('test_users.html', user_entity=user_entity)
 
 
 
