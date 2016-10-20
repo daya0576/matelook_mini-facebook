5685fa01ae1d11ca5ad79d880a3750fc4af2f5c2
daya0576 <daya0576@gmail.com>
2016-10-20 19:38:12 +1100

    testing cgi

--- matelook.py.cgi
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3.5
-
-from wsgiref.handlers import CGIHandler
-from matelook.matelook import app
-
-CGIHandler().run(app)
--- matelook/static/js/posts.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by henry on 20/10/16.
- */
-function show_comments(obj) {
-    alert($(obj).next(".comments"));
-    // $(obj).
-    $(obj).parents(".box_top").next().show();
-}
